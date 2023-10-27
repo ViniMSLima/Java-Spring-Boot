448c72d98dab8cb3e3b1c4861a4af2a8fc23326c
ViniMSLima <vinisary.lima@gmail.com>
2023-10-27 11:13:11 -0300

    exercicio_1

--- src/main/java/com/nininho/javaapi/JavaapiApplication.java
+++ src/main/java/com/nininho/javaapi/JavaapiApplication.java
@@ -1,5 +1,7 @@
 package com.nininho.javaapi;
 
+import java.util.Scanner;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +9,45 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class JavaapiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(JavaapiApplication.class, args);
+		// SpringApplication.run(JavaapiApplication.class, args);
+
+		float sum = 0;
+		float grade = 0;
+
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.print("Quantidade de notas a serem somadas\n> ");
+		int qtdNotasInt = scan.nextInt();
+
+		for (int i = 0; i < qtdNotasInt; i++)
+		{
+			System.out.print("Nota " + (i + 1) + "\n> ");
+
+			grade = scan.nextFloat();
+			sum += grade;
+		}
+
+		float mean = sum/qtdNotasInt;
+
+		System.out.println("Media: " + mean);
+
+		if (mean > 90)
+		{
+			System.out.print("Parabens!!");
+		}
+		else if (mean > 70)
+		{
+			System.out.println("Aprovado!!!");
+		}
+		else if (mean > 30) 
+		{
+			System.out.println("Recuperacao!!!");
+		} 
+		else
+		{
+			System.out.println("Reprovado!!!");
+		}
+
 	}
 
 }
