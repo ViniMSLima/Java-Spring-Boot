461eb88121ef1fac2f9c38272866085fe1e15a18
ViniMSLima <vinisary.lima@gmail.com>
2023-11-08 10:24:05 -0300

    save_intervalokk

---
+++ src/main/java/com/nininho/javaapi/model/UserModel.java
@@ -0,0 +1,28 @@
+package com.nininho.javaapi.model;
+
+import java.util.Date;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+@Document("user")
+@AllArgsConstructor
+@Data
+public class UserModel {
+    @Id
+    @Getter
+    @Setter
+    private String id;
+    @Getter
+    @Setter
+    private String name;
+    @Getter
+    @Setter
+    private short age;
+
+}
